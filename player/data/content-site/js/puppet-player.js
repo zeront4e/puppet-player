a6dd985b39c504db740ba52c6539b7e96294d689
zeront4e <zeront4e@dgs-development.de>
2022-10-17 00:42:33 +0200

    Added overlapping detection for intro image.

--- player/data/content-site/js/puppet-player.js
+++ player/data/content-site/js/puppet-player.js
@@ -77,12 +77,16 @@ createApp({
             //General GUI variables.
             guiCurrentUI: MAIN_MENU_GUI,
             guiLastMenuUI: MAIN_MENU_GUI,
+            guiIntroCoverImageWidth: 300,
 
             //Player and player GUI variables.
             playerUsesContentsIndex: true,
             player: null,
             playerLastContentId: null,
-            playerLastContentTimestamp: null
+            playerLastContentTimestamp: null,
+
+            //Global variables.
+            globalOverlapCheckIntervalId: null
         }
     },
     computed: {
@@ -227,6 +231,11 @@ createApp({
                 this.pauseBackgroundVideoIfPresent();
         },
         onBtnContinueToMainMenu: function() {
+            //Clear the overlap check interval, after a user interaction.
+            if(this.globalOverlapCheckIntervalId != null)
+                clearInterval(this.globalOverlapCheckIntervalId);
+
+            //Enter fullscreen, if desired.
             if(this.settingsEnterFullscreenAfterIntro)
                 document.body.requestFullscreen();
 
@@ -425,7 +434,7 @@ createApp({
         playContent: function(content, instantPlayback, useFullscreen, startTime, hideControls) {
             if(this.player === null) {
                 //Create a new player element here, because we assumable need the permission by the user (click event).
-                const isMobileBrowser = function() {
+                const isMobileBrowser = () => {
                     const identifierString = navigator.userAgent || navigator.vendor || window.opera;
 
                     return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(identifierString) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(identifierString.substr(0, 4));
@@ -740,6 +749,30 @@ createApp({
             //Load settings.
             this.loadSettings();
 
+            //Start automatic overlap check interval.
+            this.globalOverlapCheckIntervalId = setInterval(() => {
+                const introImageElement = document.getElementById("introImage");
+                const introButtonElement = document.getElementById("introButton");
+
+                if(introImageElement == null || introButtonElement == null)
+                    return;
+
+                const overlap = this.checkDomElementsOverlap(introImageElement, introButtonElement);
+
+                const reducedImageWith = this.guiIntroCoverImageWidth - this.guiIntroCoverImageWidth * 0.25;
+
+                if(overlap) {
+                    if(reducedImageWith > 0) {
+                        this.guiIntroCoverImageWidth = reducedImageWith;
+                    }
+                    else {
+                        clearInterval(this.globalOverlapCheckIntervalId);
+
+                        this.globalOverlapCheckIntervalId = null;
+                    }
+                }
+            }, 500);
+
             //Load contents and meta-data.
             let setupStepsCount = 0;
 
@@ -825,7 +858,7 @@ createApp({
             });
 
             //Load image/video resources.
-            const logResourceError = function(error, fileName, fileExtensionsArray, logAsWarning) {
+            const logResourceError = (error, fileName, fileExtensionsArray, logAsWarning) => {
                 let consoleFunction;
 
                 if(logAsWarning) {
@@ -965,6 +998,17 @@ createApp({
             });
 
             return promise;
+        },
+        checkDomElementsOverlap: function(firstElement, secondElement) {
+            const firstRectangle = firstElement.getBoundingClientRect();
+            const secondRectangle = secondElement.getBoundingClientRect();
+
+            const overlappingTop = firstRectangle.top > secondRectangle.bottom;
+            const overlappingBottom = firstRectangle.bottom < secondRectangle.top;
+            const overlappingLeft = firstRectangle.left > secondRectangle.right;
+            const overlappingRight = firstRectangle.right < secondRectangle.left;
+
+            return !(overlappingTop || overlappingBottom || overlappingLeft || overlappingRight);
         }
     }
 }).mount('#appContainer')
